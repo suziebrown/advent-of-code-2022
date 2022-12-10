b6f022cd904ced9486970dc227cef3b55ef219af
Suzie Brown <Suzie.Brown@ghyston.com>
2022-12-10 19:24:37 +0000

    Day 5

--- day_05.rb
+++ day_05.rb
@@ -1,5 +1,5 @@
 class Day05
-  attr_reader :configuration
+  attr_reader :stacks
   attr_reader :moves
 
   def initialize(input_filename)
@@ -8,15 +8,102 @@ class Day05
 
   def read_input_file(filename)
     configuration_and_moves = File.read(filename).split("\n\n")
-    @configuration = Stacks.new(configuration_and_moves[0])
+    @stacks = Stacks.new(configuration_and_moves[0])
     @moves = configuration_and_moves[1].split("\n")
+  end
+
+  def apply_moves_and_get_top_crates
+    apply_moves
+    get_top_crates
+  end
+
+  def apply_moves_2_and_get_top_crates
+    apply_moves_2
+    get_top_crates
+  end
+
+  def apply_moves
+    @moves.map do |move|
+      @stacks.apply_move(move)
+    end
+  end
+
+  def apply_moves_2
+    @moves.map do |move|
+      @stacks.apply_move_2(move)
+    end
+  end
+
+  def get_top_crates
+    top_crates = @stacks.crates.map {|stack| stack[-1]}
+    top_crates.join("")
   end
 end
 
 
 class Stacks
+  attr_reader :number_of_stacks
+  attr_reader :crates
+
   def initialize(input)
     lines = input.split("\n")
-    @number_of_stacks = 1 # qq
+    # only works for single-digit stack numbers
+    stack_numbers = lines.pop.gsub!(" ", "")
+
+    @number_of_stacks = Integer(stack_numbers[-1])
+    @crates = Array.new(@number_of_stacks)
+    get_initial_crate_configuration(lines.reverse!)
+  end
+
+  def get_initial_crate_configuration(crates_input)
+    for stack_number in 0..@number_of_stacks - 1 do
+      @crates[stack_number] = Stacks.get_crates_in_stack(stack_number, crates_input)
+    end
+  end
+
+  def apply_move(move)
+    number_of_crates_to_move, from_stack, to_stack = Stacks.parse_move_instruction(move)
+
+    number_of_crates_moved = 0
+    while number_of_crates_moved < number_of_crates_to_move do
+      @crates[to_stack].push(@crates[from_stack].pop)
+      number_of_crates_moved += 1
+    end
+  end
+
+  def apply_move_2(move)
+    number_of_crates_to_move, from_stack, to_stack = Stacks.parse_move_instruction(move)
+
+    @crates[to_stack].concat(@crates[from_stack].pop(number_of_crates_to_move))
+  end
+
+  def self.parse_move_instruction(move)
+    instruction_parts = move.split(" ")
+    number_of_crates_to_move = Integer(instruction_parts[1])
+    from_stack = Integer(instruction_parts[3]) - 1
+    to_stack = Integer(instruction_parts[5]) - 1
+
+    return number_of_crates_to_move, from_stack, to_stack
+  end
+
+  def self.get_crates_in_stack(stack_number, crates_input)
+    crates = Array.new()
+    column_number = Stacks.get_input_column_number(stack_number)
+
+    crates_input.map do |line|
+      if line[column_number] != nil and line[column_number] != " "
+        crates.push(line[column_number])
+      end
+    end
+
+    crates
+  end
+
+  def self.get_input_column_number(stack_number)
+    4 * stack_number + 1
   end
 end
+
+
+puts "Part 1: " + Day05.new("data/day_05_input.txt").apply_moves_and_get_top_crates
+puts "Part 2: " + Day05.new("data/day_05_input.txt").apply_moves_2_and_get_top_crates
--- day_05_test.rb
+++ day_05_test.rb
@@ -6,5 +6,80 @@ class Day05Test < Minitest::Test
     day_05 = Day05.new("data/day_05_input_test.txt")
 
     assert_equal(4, day_05.moves.length)
+    assert_equal(3, day_05.stacks.number_of_stacks)
+    assert_equal([%w[Z N], %w[M C D], %w[P]], day_05.stacks.crates)
+  end
+
+  def test__apply_moves
+    day_05 = Day05.new("data/day_05_input_test.txt")
+    day_05.apply_moves
+    assert_equal([%w[C], %w[M], %w[P D N Z]], day_05.stacks.crates)
+  end
+
+  def test__apply_moves_2
+    day_05 = Day05.new("data/day_05_input_test.txt")
+    day_05.apply_moves_2
+    assert_equal([%w[M], %w[C], %w[P Z N D]], day_05.stacks.crates)
+  end
+
+  def test__get_top_crates
+    day_05 = Day05.new("data/day_05_input_test.txt")
+    assert_equal("NDP", day_05.get_top_crates)
+  end
+
+  def test__apply_moves_and_get_top_crates
+    day_05 = Day05.new("data/day_05_input_test.txt")
+    assert_equal("CMZ", day_05.apply_moves_and_get_top_crates)
+  end
+
+  def test__apply_moves_2_and_get_top_crates
+    day_05 = Day05.new("data/day_05_input_test.txt")
+    assert_equal("MCD", day_05.apply_moves_2_and_get_top_crates)
+  end
+
+  def test__get_crates_in_stack
+    crates_0 = Stacks.get_crates_in_stack(0, ["[A] [B] [C]", "[D] [E]", "    [F]",])
+    crates_1 = Stacks.get_crates_in_stack(1, ["[A] [B] [C]", "[D] [E]", "    [F]",])
+    crates_2 = Stacks.get_crates_in_stack(2, ["[A] [B] [C]", "[D] [E]", "    [F]",])
+    assert_equal(%w[A D], crates_0)
+    assert_equal(%w[B E F], crates_1)
+    assert_equal(%w[C], crates_2)
+  end
+
+  def test__apply_move
+    day_05 = Day05.new("data/day_05_input_test.txt")
+
+    day_05.stacks.apply_move("move 1 from 2 to 1")
+    assert_equal([%w[Z N D], %w[M C], %w[P]], day_05.stacks.crates)
+
+    day_05.stacks.apply_move("move 3 from 1 to 3")
+    assert_equal([[], %w[M C], %w[P D N Z]], day_05.stacks.crates)
+
+    day_05.stacks.apply_move("move 2 from 2 to 1")
+    assert_equal([%w[C M], [], %w[P D N Z]], day_05.stacks.crates)
+
+    day_05.stacks.apply_move("move 1 from 1 to 2")
+    assert_equal([%w[C], %w[M], %w[P D N Z]], day_05.stacks.crates)
+  end
+
+  def test__apply_move_2
+    day_05 = Day05.new("data/day_05_input_test.txt")
+
+    day_05.stacks.apply_move_2("move 1 from 2 to 1")
+    assert_equal([%w[Z N D], %w[M C], %w[P]], day_05.stacks.crates)
+
+    day_05.stacks.apply_move_2("move 3 from 1 to 3")
+    assert_equal([[], %w[M C], %w[P Z N D]], day_05.stacks.crates)
+
+    day_05.stacks.apply_move_2("move 2 from 2 to 1")
+    assert_equal([%w[M C], [], %w[P Z N D]], day_05.stacks.crates)
+
+    day_05.stacks.apply_move_2("move 1 from 1 to 2")
+    assert_equal([%w[M], %w[C], %w[P Z N D]], day_05.stacks.crates)
+  end
+
+  def test__parse_move_instruction
+    assert_equal([1, 1, 0], Stacks.parse_move_instruction("move 1 from 2 to 1"))
+    assert_equal([12, 8, 3], Stacks.parse_move_instruction("move 12 from 9 to 4"))
   end
 end
