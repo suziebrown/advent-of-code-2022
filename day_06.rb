d7b0793ce2a1d103ea99dd6d64f7edd3e18545d6
Suzie Brown <Suzie.Brown@ghyston.com>
2022-12-11 10:55:34 +0000

    Day 6

--- day_06.rb
+++ day_06.rb
@@ -1,54 +1,65 @@
 class Day06
+  @@end_of_packet_marker_length = 4
+  @@start_of_packet_marker_length = 14
+
   attr_reader :input
-  attr_reader :last_four_characters_tally
-  attr_writer :last_four_characters_tally
 
   def initialize(input_filename)
     @input = File.read(input_filename)
-    @last_four_characters_tally = @input[0..3].split(//).tally
+    @last_four_characters = @input[0..@@end_of_packet_marker_length - 1].split(//)
+    @last_fourteen_characters = @input[0..@@start_of_packet_marker_length - 1].split(//)
   end
 
   def get_first_packet_end
-    if Day06.packet_end?(@last_four_characters_tally)
-      return 3
+    if packet_end?
+      return @@end_of_packet_marker_length - 1
     end
 
-    position = 4
-    until Day06.packet_end?(@last_four_characters_tally) do
+    position = @@end_of_packet_marker_length
+    until packet_end? do
       if position >= @input.length
         throw("Searched entire string but didn't find end of packet")
       end
 
-      character_to_add = @input[position]
-      character_to_remove = @input[position - 3]
-
-      update_tally(character_to_add, character_to_remove)
+      @last_four_characters = @last_four_characters[1..3].push(@input[position])
       position += 1
     end
 
     position
   end
 
-  def update_tally(character_to_add, character_to_remove)
-    if character_to_add == character_to_remove
-      return
+  def get_first_packet_start
+    if packet_start?
+      return @@start_of_packet_marker_length - 1
     end
 
-    @last_four_characters_tally.update { |char,count| [char == character_to_remove] ? count - 1 : count }
+    position = @@start_of_packet_marker_length
+    until packet_start? do
+      if position >= @input.length
+        throw("Searched entire string but didn't find start of packet")
+      end
 
-    if @last_four_characters_tally.has_key? character_to_add
-      @last_four_characters_tally.update { |char,count| [char == character_to_add] ? count + 1 : count }
-    else
-      @last_four_characters_tally.update({ character_to_add => 1 })
+      @last_fourteen_characters = @last_fourteen_characters[1..13].push(@input[position])
+      position += 1
     end
+
+    position
   end
 
-  def self.packet_end?(last_four_characters_tally)
-    last_four_characters_tally.all? { |character, count| count < 2 }
+  def packet_end?
+    Day06.all_distinct?(@last_four_characters)
+  end
+
+  def packet_start?
+    Day06.all_distinct?(@last_fourteen_characters)
+  end
+
+  def self.all_distinct?(array)
+    array.tally.all? { |char, count| count < 2 }
   end
 end
 
 
 day_06 = Day06.new("data/day_06_input.txt")
 puts "Part 1: " + day_06.get_first_packet_end.to_s
-# puts "Part 2: " + day_06.to_s
+puts "Part 2: " + day_06.get_first_packet_start.to_s
--- day_06_test.rb
+++ day_06_test.rb
@@ -7,7 +7,7 @@ class Day06Test < Minitest::Test
     assert_equal("mjqjpqmgbljsphdztnvjfqwrcgsmlb", day_06.input)
   end
 
-  def test__find_first_packet_end
+  def test__get_first_packet_end
     assert_equal(7, Day06.new("data/day_06_input_test_1.txt").get_first_packet_end)
     assert_equal(5, Day06.new("data/day_06_input_test_2.txt").get_first_packet_end)
     assert_equal(6, Day06.new("data/day_06_input_test_3.txt").get_first_packet_end)
@@ -15,16 +15,19 @@ class Day06Test < Minitest::Test
     assert_equal(11, Day06.new("data/day_06_input_test_5.txt").get_first_packet_end)
   end
 
-  def test__update_tally
-    day_06 = Day06.new("data/day_06_input_test_1.txt")
-    day_06.last_four_characters_tally = Hash.new({ "a" => 0, "b" => 1, "c" => 2, "d" => 1 })
-
-    day_06.update_tally("b", "d")
-    assert_equal(0, day_06.last_four_characters_tally["a"])
-    assert_equal({ "a" => 0, "b" => 2, "c" => 2, "d" => 0 }, day_06.last_four_characters_tally)
+  def test__get_first_packet_start
+    assert_equal(19, Day06.new("data/day_06_input_test_1.txt").get_first_packet_start)
+    assert_equal(23, Day06.new("data/day_06_input_test_2.txt").get_first_packet_start)
+    assert_equal(23, Day06.new("data/day_06_input_test_3.txt").get_first_packet_start)
+    assert_equal(29, Day06.new("data/day_06_input_test_4.txt").get_first_packet_start)
+    assert_equal(26, Day06.new("data/day_06_input_test_5.txt").get_first_packet_start)
   end
 
-  def test__packet_end
-    assert_equal(true, Day06.packet_end?(Hash.new({ "a" => 1, "b" => 1, "c" => 1, "d" => 1 })))
+  def test__all_distinct
+    assert_equal(true, Day06.all_distinct?(%w[a b c d]))
+
+    assert_equal(false, Day06.all_distinct?(%w[a b c c]))
+    assert_equal(false, Day06.all_distinct?(%w[a b c a]))
+    assert_equal(false, Day06.all_distinct?(%w[a a a a]))
   end
 end
