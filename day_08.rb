a68449a44ac9e0120e4929560b4803daeeb20206
Suzie Brown <Suzie.Brown@ghyston.com>
2022-12-13 15:07:07 +0000

    Day 8 Part 2

--- day_08.rb
+++ day_08.rb
@@ -16,6 +16,14 @@ class Day08
     get_number_of_trees_on_perimeter + test__get_number_of_visible_interior_trees
   end
 
+  def get_highest_scenic_score
+    row_maxes = (1..@number_of_columns - 2).map do |row|
+      (1..@number_of_rows - 2).map { |column| get_scenic_score(row, column) }.max
+    end
+
+    row_maxes.max
+  end
+
   def get_number_of_trees_on_perimeter
     2 * (@number_of_rows + @number_of_columns) - 4
   end
@@ -66,9 +74,62 @@ class Day08
 
     !trees_to_bottom.any? { |other_tree| other_tree >= height }
   end
+
+  def get_scenic_score(row, column)
+    if  row == 0 || row == @number_of_columns - 1 || column == 0 || column == @number_of_columns - 1
+      return 0
+    end
+
+    get_viewing_distance_left(row, column) \
+    * get_viewing_distance_right(row, column) \
+    * get_viewing_distance_top(row, column) \
+    * get_viewing_distance_bottom(row, column)
+  end
+
+  def get_viewing_distance_left(row, column)
+    height = @trees[row][column]
+    trees_to_left = @trees[row][..column - 1]
+
+    Day08.get_viewing_distance_for_line_of_trees(height, trees_to_left.reverse)
+  end
+
+  def get_viewing_distance_right(row, column)
+    height = @trees[row][column]
+    trees_to_right = @trees[row][column + 1..]
+
+    Day08.get_viewing_distance_for_line_of_trees(height, trees_to_right)
+  end
+
+  def get_viewing_distance_top(row, column)
+    height = @trees[row][column]
+    trees_to_top = @trees[..row - 1].map {|row_of_trees| row_of_trees[column] }
+
+    Day08.get_viewing_distance_for_line_of_trees(height, trees_to_top.reverse)
+  end
+
+  def get_viewing_distance_bottom(row, column)
+    height = @trees[row][column]
+    trees_to_bottom = @trees[row + 1..].map {|row_of_trees| row_of_trees[column] }
+
+    Day08.get_viewing_distance_for_line_of_trees(height, trees_to_bottom)
+  end
+
+  def self.get_viewing_distance_for_line_of_trees(max_height, trees)
+    viewing_distance = 0
+
+    trees.map do |tree|
+      if tree >= max_height
+        return viewing_distance + 1
+      end
+
+      viewing_distance += 1
+    end
+
+    viewing_distance
+  end
 end
 
 
 day_08 = Day08.new("data/day_08_input.txt")
 puts "Part 1: " + day_08.get_number_of_visible_trees.to_s
-# puts "Part 2: " + day_08.
+puts "Part 2: " + day_08.get_highest_scenic_score.to_s
--- day_08_test.rb
+++ day_08_test.rb
@@ -14,6 +14,11 @@ class Day08Test < Minitest::Test
     assert_equal(21, day_08.get_number_of_visible_trees)
   end
 
+  def test__get_highest_scenic_score
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(8, day_08.get_highest_scenic_score)
+  end
+
   def test__get_number_of_trees_on_perimeter
     day_08 = Day08.new("data/day_08_input_test.txt")
     assert_equal(16, day_08.get_number_of_trees_on_perimeter)
@@ -65,5 +70,47 @@ class Day08Test < Minitest::Test
     day_08 = Day08.new("data/day_08_input_test.txt")
     assert_equal(true, day_08.tree_visible_from_bottom?(3, 2))
     assert_equal(false, day_08.tree_visible_from_bottom?(3, 3))
+  end
+
+  def test__get_scenic_score__tree_is_on_perimeter
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(0, day_08.get_scenic_score(0,2))
+    assert_equal(0, day_08.get_scenic_score(4,2))
+    assert_equal(0, day_08.get_scenic_score(2,0))
+    assert_equal(0, day_08.get_scenic_score(2,4))
+  end
+
+  def test__get_scenic_score__tree_is_not_on_perimeter
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(4, day_08.get_scenic_score(1,2))
+    assert_equal(8, day_08.get_scenic_score(3,2))
+  end
+
+  def test__get_viewing_distance_left
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(1, day_08.get_viewing_distance_left(1, 2))
+  end
+
+  def test__get_viewing_distance_right
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(2, day_08.get_viewing_distance_right(1, 2))
+  end
+
+  def test__get_viewing_distance_top
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(1, day_08.get_viewing_distance_top(1, 2))
+  end
+
+  def test__get_viewing_distance_bottom
+    day_08 = Day08.new("data/day_08_input_test.txt")
+    assert_equal(2, day_08.get_viewing_distance_bottom(1, 2))
+  end
+
+  def test__get_viewing_distance_for_line_of_trees
+    assert_equal(0, Day08.get_viewing_distance_for_line_of_trees(5, []))
+    assert_equal(3, Day08.get_viewing_distance_for_line_of_trees(5, [1, 2, 3]))
+    assert_equal(2, Day08.get_viewing_distance_for_line_of_trees(5, [1, 6, 3]))
+    assert_equal(3, Day08.get_viewing_distance_for_line_of_trees(5, [3, 2, 1]))
+    assert_equal(1, Day08.get_viewing_distance_for_line_of_trees(5, [5, 1, 2]))
   end
 end
